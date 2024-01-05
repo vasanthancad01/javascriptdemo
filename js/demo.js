8ed29fac577faf37a463e30acee84bcb41fd9603
vasanthancad01 <vasanthcad01@gmail.com>
2024-01-05 18:07:32 +0530

    files updated

--- js/demo.js
+++ js/demo.js
@@ -38,3 +38,117 @@ console.log(b);
 
 var a=[100,200];
 console.table(a);
+
+var a=[10,20,30,40];
+console.log(typeof a);
+
+var a="abc";
+console.log(typeof a);
+a=parseInt(a);
+console.log(a, typeof a);
+
+var q="abc";
+p=Number(q);
+console.log(typeof p);
+
+//additon operator 
+
+var f1=30;
+var f2=20;
+console.log(f1+f2);
+
+//subtraction operator
+ 
+var f1=30;
+var f2=20;
+console.log(f1-f2);
+
+//multiplication operator
+
+var f1=30;
+var f2=20;
+console.log(f1*f2);
+
+//division operator
+
+var f1=30;
+var f2=20;
+console.log(f1/f2);
+
+//expo operator 
+var f1=30;
+var f2=20;
+console.log(f1**f2);
+
+//modulus operator
+var f1=30;
+var f2=20;
+console.log(f1%f2);
+
+//post increment operator
+var s=10;
+s++;
+console.log(s);
+
+//post decrement operator 
+var s=11;
+s--;
+console.log(s);
+
+//pre increment operator
+var s=12;
+++s;
+console.log(s);
+
+//pre decrement operator 
+var s=13;
+--s;
+console.log(s);
+
+
+//comparison operator
+// = operator 
+var f1=30;
+var f2=20;
+console.log(f1==f2);
+
+// != operator
+var f1=30;
+var f2=20;
+console.log(f1!=f2);
+
+//datatype comparison
+var f1=30;
+var f2=20;
+console.log(f1===f2);
+
+//
+var f1=30;
+var f2=20;
+console.log(f1!==f2);
+
+//greater or less than
+var f1=30;
+var f2=20;
+console.log(f1>f2);
+console.log(f1<f2);
+
+//greater or less than eqaul too
+var f1=30;
+var f2=20;
+console.log(f1>=f2);
+console.log(f1<=f2);
+
+//ternary operator 
+var f1=30;
+var f2=20;
+console.log(f1>f2?"car":"bike");
+
+//
+var f1="car";
+var f2="bike";
+var result= f1 > f2;
+console.log(result);
+
+
+
