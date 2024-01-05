5c9f8f803a7c19608d58084a27a57fca9a9b7c7f
vasanthancad01 <vasanthcad01@gmail.com>
2024-01-05 15:14:00 +0530

    js

---
+++ js/demo.js
@@ -0,0 +1,40 @@
+
+//welcome message using string
+console.log("welcome to great kirikalan magic show")
+//addition of two numbers
+var a=10;
+let b=20;
+console.log(a-b);
+//function demo inside function  
+function addition(){
+
+var a=15;
+let b=30;
+const c=30;
+
+console.log(b);
+}
+addition()
+
+//function demo outside function
+
+function addition1(){
+
+var a=80;
+let b=90;
+const c=100;
+}
+addition()
+
+console.log(b);
+
+//array in table 
+a=[10,20,30,40];
+console.table(a);
+
+//array in log 
+b=[50,60,70,80];
+console.log(b);
+
+var a=[100,200];
+console.table(a);
